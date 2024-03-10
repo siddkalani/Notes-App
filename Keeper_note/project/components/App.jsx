e5f8fe5ab84ee5edaaa2c2d37b26b8fd26b7715f
siddkalani <143438044+siddkalani@users.noreply.github.com>
2024-03-10 21:59:08 +0530

    Add files via upload

---
+++ Keeper_note/project/components/App.jsx
@@ -0,0 +1,74 @@
+// import React from "react";
+// import Header from "./Header";
+// import Footer from "./Footer";
+// import Note from "./Note";
+// import addings from "../addings";
+
+
+// function App() {
+//   return (
+//     <div>
+//       <Header />
+//      {addings.map(NoteEntry=>(
+// <Note
+// key={NoteEntry.id}
+// title={NoteEntry.title}
+// content={NoteEntry.content}
+// />
+//      ))}
+     
+//       <Footer />
+//     </div>
+//   );
+//      }
+
+// export default App;
+
+import React ,{ useState } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
+
+
+
+
+function App() {
+  const[notes , setNotes] = useState([])
+
+  function addNote(newNote){
+    setNotes(prevValue=>{
+     return [...prevValue, newNote]
+  })
+
+  }
+   
+  function deleteNote(id){
+setNotes(prevNotes=>{
+  return prevNotes.filter((noteItem,index)=>{
+    return index!==id
+  })
+})
+  }
+  
+  return (
+    <div>
+      <Header />
+      <CreateArea onAdd={addNote}/>
+      {notes.map((noteItem,index)=>
+      {
+        return(  
+        <Note
+        id = {index}
+        key = {index}
+         title={noteItem.title}
+        content={noteItem.content}
+        onDelete={deleteNote}
+        />)
+      })}
+      <Footeefr />ef
+    </div>
+  );
+}
+
+export default App;
